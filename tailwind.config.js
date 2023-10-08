a6a32b13a5908fa989414db96db15dd0a4dbd47a
elham saremi <elhamsaremi88@gmail.com>
2023-10-08 14:59:00 +0330

    feat:tailwind home-page-1 and change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,24 @@ module.exports = {
 
   theme: {
     extend: {
+      colors: {
+        title: {
+          midumGray: "#757475",
+          HeadingTitleColor: "#152536",
+          dark: "#343A40",
+        },
+      },
       fontSize: {
         heading1: "72px",
         heading2: "50px",
         heading3: "40px",
         text1: "32px",
+        tinyText: "10px",
       },
-      backgroundImage: {
-        bg_image: "url('asset/images/shoes1.jpg')",
-      },
+      // backgroundImage: {
+      //   bg_image: "url('asset/images/shoes1.jpg')",
+      // },
     },
   },
-  // corePlugins: {
-  //   preflight: false,
-  // },
-  darkMode: "class",
+  plugins: [require("tailwind-scrollbar-hide")],
 };
